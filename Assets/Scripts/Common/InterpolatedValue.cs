0897cea6fca5e496ab3a85d763d8fbc3c77f8ce3
Bui Trong Nhan <imouto1994@gmail.com>
2015-02-18 22:29:51 +0700

    Add common classes

---
+++ Assets/Scripts/Common/InterpolatedValue.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterpolatedValue : MonoBehaviour {	
+
+	public int startDistance;
+	public float startValue;
+	public int endDistance;
+	public float endValue;
+	public bool hasLimit;
+	
+	public InterpolatedValue(int sd, float sv, int ed, float ev, bool ued)
+	{
+		startDistance = sd;
+		startValue = sv;
+		endDistance = ed;
+		endValue = ev;
+		hasLimit = ued;
+	}
+	
+	public float GetInterpolatedValue(float distance)
+	{
+		// if the distance is before the start distance or after the end distance, return 0
+		if (distance < startDistance || (hasLimit && distance > endDistance))
+			return 0;
+		
+		if (startDistance == endDistance || !hasLimit) {
+			return startValue;
+		}
+		
+		float distancePercent = ((distance - startDistance) / (endDistance - startDistance));
+		
+		// distancePercent can be greater than 1 if distance > endDistance
+		if (distancePercent > 1)
+			distancePercent = 1;
+		
+		// linear interpolation
+		return startValue + (distancePercent * (endValue - startValue));
+	}
+	
+	public bool IsWithinRange(float distance)
+	{
+		return distance >= startDistance && (!hasLimit || (hasLimit && distance <= endDistance));
+	}
+
+}
---
+++ Assets/Scripts/Common/InterpolatedValueList.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InterpolatedValueList
+{
+	// this array MUST be in order according to startDistance/EndDistance, with no overlap
+	public List<InterpolatedValue> values;
+	
+	public bool hasLoop;
+	public int loopBackToIndex = 0;	
+	private List<InterpolatedValue> loopedValues;
+
+	private int lastEndDistanceIndex;
+	private int[] endDistances;
+	private int[] loopedEndDistances;
+	
+	public InterpolatedValueList(bool l, int index)
+	{
+		hasLoop = l;
+		loopBackToIndex = index;
+	}
+	
+	public InterpolatedValueList(InterpolatedValue v)
+	{
+		values = new List<InterpolatedValue>();
+		values.Add(v);
+	}
+	
+	public void Init()
+	{
+		endDistances = new int[values.Count];
+		lastEndDistanceIndex = 0;
+		for (int i = 0; i < endDistances.Length; ++i) {
+			endDistances[i] = values[i].endDistance;
+		}
+		if (hasLoop) {
+			loopedValues = new List<InterpolatedValue>();
+			int startDistance = values[loopBackToIndex].startDistance;
+			loopedEndDistances = new int[values.Count - loopBackToIndex];
+			for (int i = loopBackToIndex; i < values.Count; ++i) {
+				loopedValues.Add(new InterpolatedValue(values[i].startDistance - startDistance, 
+				                                       values[i].startValue, 
+				                                       values[i].endDistance - startDistance, 
+				                                       values[i].endValue, 
+				                                       values[i].hasLimit));
+				loopedEndDistances[loopedValues.Count - 1] = loopedValues[loopedValues.Count - 1].endDistance;
+			}
+		}
+	}
+	
+	public void ResetValues()
+	{
+		lastEndDistanceIndex = 0;
+	}
+	
+	public int Count()
+	{
+		if (values == null)
+			return 0;
+		return values.Count;
+	}
+	
+	public float GetValue(float distance)
+	{
+		if (values.Count == 0)
+			return 0;
+		
+		bool looped = false;
+		if (hasLoop) {
+			float prevDistance = distance;
+			looped = (int)(distance / values[endDistances.Length - 1].endDistance) > 0;
+			if (looped) {
+				distance = (distance - values[loopBackToIndex].startDistance) % loopedValues[loopedEndDistances.Length - 1].endDistance;
+			} else {
+				distance = distance % values[endDistances.Length - 1].endDistance;
+			}
+			// reset lastEndDistanceIndex if the distance looped around
+			if (distance <= prevDistance) {
+				lastEndDistanceIndex = 0;
+			}
+		}
+		
+		if (looped) {
+			return loopedValues[GetIndexFromDistance(distance, true)].GetInterpolatedValue(distance);
+		} else {
+			return values[GetIndexFromDistance(distance, false)].GetInterpolatedValue(distance);
+		}
+	}
+	
+	public void GetMinMaxValue(out float min, out float max)
+	{
+		max = float.MinValue;
+		min = float.MaxValue;
+		for (int i = 0; i < values.Count; ++i) {
+			if (max < values[i].startValue) {
+				max = values[i].startValue;
+			}
+			if (min > values[i].startValue) {
+				min = values[i].startValue;
+			}
+			if (values[i].hasLimit) {
+				if (max < values[i].endValue) {
+					max = values[i].endValue;
+				}
+				if (min > values[i].endValue) {
+					min = values[i].endValue;
+				}
+			}
+		}
+	}
+	
+	// Don't loop from the start of the list each time. Keep a cache of the latest index. This works because the 
+	// list is in order from the start distance to the end distance.
+	private int GetIndexFromDistance(float distance, bool useLoopedEndDistances)
+	{
+		int[] distances = useLoopedEndDistances ? loopedEndDistances : endDistances;
+		for (int i = lastEndDistanceIndex; i < distances.Length; ++i) {
+			if (distance <= distances[i]) {
+				lastEndDistanceIndex = i;
+				return i;
+			}
+		}
+		
+		return distances.Length - 1;
+	}
+}
