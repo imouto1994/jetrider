180caf533c1779cb6473adc0f9f3c5b3d4fd7d4c
Sai Hougit config --global user.email sai-hou@hotmail.com <chue.sai.hou@gmail.com>
2015-02-22 21:39:23 +0800

    Added Welcome screen and Fuel UI.

---
+++ Assets/Scripts/Controllers/FuelTracker.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FuelTracker : MonoBehaviour {
+
+	public Slider fuelBar;
+
+	private float fuel;
+	private float maxFuel;
+	private float minFuel;
+
+	// Use this for initialization
+	void Start () {
+		fuel = 0.0f;
+		maxFuel = 100.0f;
+		minFuel = 0.0f;
+		UpdateFuelBar();
+	}
+
+	public void IncreaseFuel(float fuelGain) {
+		Debug.Log ("Player picked up fuel");
+		fuel += fuelGain;
+
+		if (fuel > maxFuel) {
+			fuel = maxFuel;
+		}
+		UpdateFuelBar();
+	}
+
+	public void DecreaseFuel(float fuelDrain) {
+		Debug.Log ("Draining fuel");
+		fuel -= fuelDrain;
+
+		if (fuel < minFuel) {
+			fuel = minFuel;
+		}
+		UpdateFuelBar();
+	}
+
+	private void UpdateFuelBar() {
+		fuelBar.value = (fuel / 100.0f);
+	}
+}
