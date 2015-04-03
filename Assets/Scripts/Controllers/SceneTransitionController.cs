e8b41c86e4b5c9116154cd8b294d76cb0cbb3fd1
saihou <sai-hou@hotmail.com>
2015-04-03 15:16:53 +0800

    Made fuel bar prefab not interactable Group scene transitions code under 1 C# script.

---
+++ Assets/Scripts/Controllers/SceneTransitionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransitionController : MonoBehaviour {
+
+	public void QuitGame() {
+		Debug.Log ("Quit game");
+		Application.Quit ();
+	}
+
+	public void StartGame() {
+		Debug.Log ("Play game!");
+		Application.LoadLevel("LevelTest");
+	}
+
+	public void RestartGame() {
+		Debug.Log ("Restart game");
+		Application.LoadLevel("LevelTest");
+	}
+
+	public void ReturnToMainMenu() {
+		Debug.Log ("Return to main menu");
+		Application.LoadLevel("Welcome");
+	}
+}
