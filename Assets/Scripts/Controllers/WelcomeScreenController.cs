b6703911522938270e584f869f7b5b9369c65c4c
saihou <sai-hou@hotmail.com>
2015-04-16 19:09:54 +0800

    Finished implementing instructions menu. This closes #3

---
+++ Assets/Scripts/Controllers/WelcomeScreenController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WelcomeScreenController : MonoBehaviour {
+
+	public GameObject instructionsScreen;
+
+	public void DisplayInstructions() {
+		instructionsScreen.SetActive(true);
+	}
+	
+	public void CloseInstructions() {
+		instructionsScreen.SetActive(false);
+	}
+
+	public void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (instructionsScreen.activeSelf) {
+				CloseInstructions();
+			}
+		}
+	}
+}
