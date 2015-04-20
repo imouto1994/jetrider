2eca171f758a242898014e299613c64f936fe462
saihou <sai-hou@hotmail.com>
2015-04-20 16:08:02 +0800

    Implemented saving of high score. This closes #4 Renamed LevelTest.unity to Level.unity

--- Assets/Scripts/Controllers/HighScoreController.cs
+++ Assets/Scripts/Controllers/HighScoreController.cs
@@ -7,6 +7,14 @@ public class HighScoreController : MonoBehaviour {
 	public Text highScoreText;
 	
 	void OnEnable() {
+		UpdateHighScore();
+	}
+
+	void UpdateHighScore() {
 		highScoreText.text = PlayerPrefs.GetInt ("High Score").ToString();
+	}
+	public void ResetHighScore() {
+		PlayerPrefs.SetInt("High Score", 0);
+		UpdateHighScore();
 	}
 }
